138e2814d6b983ea5b13c77f75a3ab451235847a
will <william.bordeau@hotmail.fr>
2021-05-26 03:47:56 -0400

    changement meme file

--- potager/cueillir.sh
+++ potager/cueillir.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "belle recolte"
 echo "pour la soupe aux choux, mais pas que !"
-echo "on pourra faire plein d'autres choses, c'est sûr !"
+echo "on pourra faire plein d'autres choses !"
