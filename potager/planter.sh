de1a04fa48b229bd9cd7989b8fd5b30da5c54be4
will <william.bordeau@hotmail.fr>
2021-05-26 03:53:12 -0400

    modif essai 2

--- potager/planter.sh
+++ potager/planter.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-echo "planter les "
-=======
 echo "savez-vous..."
-echo "planter les choux"
->>>>>>> 11a8715720e6296c34fed15f1c35c44073eb099c
-echo "a la mode chez nous"
-echo "planter des radis"
+echo "savez-vous rien faire"
